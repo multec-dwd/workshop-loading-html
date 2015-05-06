a71c8c32d5990f0f12cf5611731c4692224a950c
jnaklaas <JK@boo.local>
2015-05-06 11:07:50 +0200

    Added loading subpages

--- script-subpage.js
+++ script-subpage.js
@@ -3,6 +3,7 @@ devel: true,
 browser: true,
 jquery: true
 */
+var subpages = []; //globale array met URL's van de subpages
 
 $(document).ready(function() {
     var sendData = {url: 'http://www.abconcerts.be/nl/agenda/'};
@@ -22,6 +23,7 @@ var successHandler = function(data) {
     
     var htmlString = '';
     
+    
     $html.find('.item').each(function(i, eventItem){
         
         var $eventItem = $(eventItem);
@@ -29,13 +31,47 @@ var successHandler = function(data) {
                         .replace(/overview/, 'header')
                         .replace(/small/,'large');
         var date = $eventItem.parent().parent().prev().text();
+        var urlSubpage = $eventItem.attr('href');
+        subpages.push(urlSubpage);
+        
         
         htmlString += '<section class="event">'; //style="background: url(' + imgSrc +');">';
         htmlString += '<img src="' + imgSrc + '">';
         htmlString += '<h1>' + $eventItem.find('.title').text() + '</h1>';
         htmlString += '<h2>' + date + '</h2>';
+        //htmlString += '<p>' + genre + '</p>'; //genre zit in een subpagina, dus kan hier niet worden toegevoegd
         htmlString += '</section>';
+        
+        loadSubPage(urlSubpage);
     });
     
     $(document.body).append(htmlString);
+};
+
+var loadSubPage = function(urlSubpage){
+    var sendData = {url: urlSubpage};
+    $.ajax({
+        url: 'crosscall.php',
+        data: sendData,
+        type: 'POST',
+        error: function(err) {console.dir(err);},
+        success: successHandlerSubPage
+    });
+};
+
+var successHandlerSubPage = function(data){
+    var loadedUrl = this.data.substr(4); //url huidige ajax request
+    loadedUrl = decodeURIComponent(loadedUrl).replace(/\++/g, ' '); //decoding special chars in url
+    var index;
+    
+    $.each(subpages, function(i, urlSubpage){
+        if(loadedUrl === urlSubpage) {
+            index = i;
+            return false; //indien gevonden, stop each loop
+        }
+    });
+    
+    var $html = $(data);
+    var genre = $html.find(".info:contains('Genre')").html();
+    $(".event").eq(index).append('<p>' + genre + '</p>');
 };
